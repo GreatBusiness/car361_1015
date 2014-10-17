ce4afe2448a1fe24c636646fc3bf1430bb4fa9b4
fblife2014 <shizhongkun@fblife.com>
2014-10-18 00:48:03 +0800

    x
    x

---
+++ car361/car361/viewControllers/商家/model/FuwujieshaoModel.m
@@ -0,0 +1,38 @@
+//
+//  FuwujieshaoModel.m
+//  car361
+//
+//  Created by szk on 14-10-17.
+//  Copyright (c) 2014年 lcw. All rights reserved.
+//
+
+#import "FuwujieshaoModel.h"
+
+@implementation FuwujieshaoModel
+
+-(FuwujieshaoModel *)initWithDictionary:(NSDictionary *)dic
+{
+    self = [super init];
+    
+    if (self) {
+        
+        _thedetailid=[NSString stringWithFormat:@"%@",[dic objectForKey:@"id"]];
+        
+        _name=[NSString stringWithFormat:@"%@",[dic objectForKey:@"name"]];
+        _cid=[NSString stringWithFormat:@"%@",[dic objectForKey:@"cid"]];
+        _shopid=[NSString stringWithFormat:@"%@",[dic objectForKey:@"shopid"]];
+        _otherid=[NSString stringWithFormat:@"%@",[dic objectForKey:@"otherid"]];
+        _price=[NSString stringWithFormat:@"%@",[dic objectForKey:@"price"]];
+        _score=[NSString stringWithFormat:@"%@",[dic objectForKey:@"score"]];
+        _trade=[NSString stringWithFormat:@"%@",[dic objectForKey:@"trade"]];
+        
+        _content=[NSString stringWithFormat:@"%@",[dic objectForKey:@"content"]];
+        
+        
+    }
+    
+    return self;
+}
+
+
+@end
